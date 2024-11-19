19dd5203d16a28afae3131179d93a671a8592962
seeds-morishita <r.morishita@seeds-std.co.jp>
2024-11-19 16:16:27 +0900

    新規投稿機能のコードを一番上に移しました

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -18,6 +18,33 @@ class ArticleController extends Controller
         // ビューに$articlesを渡す
         return view('articles.index', compact('articles'));
     }
+
+    /** 新規投稿機能 */
+    public function postConfirm(Article $article)
+    {
+        Session::put('id', $article->id);
+    
+        $token = strval(time());
+        Session::put('token', $token);
+    
+        return view('articles.post_confirm', compact('article'));
+    }
+    
+    public function postComplete(PostRequest $request, Article $article)
+    {
+        $article->name = $request->input('name');
+        $article->content = $request->input('content');
+        
+        // データベースに保存
+        $article->save();
+        
+        // セッション内のデータ削除
+        Session::forget('id');
+        Session::forget('token');
+        
+        return view('articles.post_complete');
+    }
+
     /** 編集機能 */
     public function edit(Article $article)
     {
@@ -46,34 +73,6 @@ class ArticleController extends Controller
     
         return view('articles.edit_complete');
     }
-
-
-    /** 新規投稿機能 */
-    public function postConfirm(Article $article)
-    {
-        Session::put('id', $article->id);
-
-        $token = strval(time());
-        Session::put('token', $token);
-
-        return view('articles.post_confirm', compact('article'));
-    }
-
-    public function postComplete(PostRequest $request, Article $article)
-    {
-        $article->name = $request->input('name');
-        $article->content = $request->input('content');
-        
-        // データベースに保存
-        $article->save();
-        
-        // セッション内のデータ削除
-        Session::forget('id');
-        Session::forget('token');
-        
-        return view('articles.post_complete');
-    }
-    
 
     /** 削除機能 */
     public function deleteConfirm(Article $article)
