c9e3557538c13685df8436abf2da545ad498fa11
seeds-morishita <r.morishita@seeds-std.co.jp>
2025-02-18 16:33:31 +0900

    不要なコードを削除しました

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Article;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,10 +12,5 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(ArticlesTableSeeder::class);
-
-        Article::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
