65e7286ef462d75a452187f840a0142b13bfb471
seeds-morishita <r.morishita@seeds-std.co.jp>
2025-02-26 17:46:02 +0900

    entrypointの修正しました

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+# composerをインストール
+composer install --no-interaction
+
+# アプリケーションキーを生成
+php artisan key:generate
+
 # マイグレーションを実行
 php artisan migrate --force
 
 # シーダーを実行
 php artisan db:seed --force
 
-# アプリケーションキーを生成
-php artisan key:generate
 
 apache2-foreground
